29cbda54ec36e8c0ccc89167b065fe5eef855ad4
swethalakshmi22 <59462209+swethalakshmi22@users.noreply.github.com>
2020-04-10 19:37:29 +0530

    completed

--- app.js
+++ app.js
@@ -1,63 +1,86 @@
 //All input-fields
 
-const firstName = document.getElementById('input1');
-// const lastName = document.getElementById('input2');
-// const emailAdd = document.getElementById('input3');
-// const passwordInput = document.getElementById('input4');
+const inputField1 = document.getElementById('input1');
+const inputField2 = document.getElementById('input2');
+const inputField3 = document.getElementById('input3');
+const inputField4 = document.getElementById('input4');
 
 //All errorimgs
 
-const errorImg1 = document.getElementById('errorimg1');
-// const errorImg2 = document.getElementById('errorimg2');
-// const errorImg3 = document.getElementById('errorimg3');
-// const errorImg4 = document.getElementById('errorimg4');
+const errorImg1 = document.getElementById('error-img1');
+const errorImg2 = document.getElementById('error-img2');
+const errorImg3 = document.getElementById('error-img3');
+const errorImg4 = document.getElementById('error-img4');
 
 //All errormgs
 
 const errorMsg1 = document.getElementById('error-msg1');
-// const errorMsg2 = document.getElementById('error-msg2');
-// const errorMsg3 = document.getElementById('error-msg3');
-// const errorMsg4 = document.getElementById('error-msg4');
+const errorMsg2 = document.getElementById('error-msg2');
+const errorMsg3 = document.getElementById('error-msg3');
+const errorMsg4 = document.getElementById('error-msg4');
 
-
-document.getElementsByTagName('button').addEventListener('click', e => {
-  e.preventDefault();
-  let firstVal = firstName.value.trim();
-  // let lastVal = lastName.value.trim();
-  // let emailVal = emailAdd.value.trim();
-  // let password = passwordInput.value.trim();
-  
-  if(firstVal === '') {
-    firstVal = '';
-    firstName.focus();
-    errorImg1.style.display = 'block';
-    errorMsg1.innerHTML = 'First Name cannot be empty.';
+function inputErrors(event) {
+   let inputVal1 = inputField1.value.trim();
+   let inputVal2 = inputField2.value.trim();
+   let inputVal3 = inputField3.value.trim();
+   let inputVal4 = inputField4.value.trim();
+  if (inputVal1 === '') {
+     inputVal1 === '';
+     inputField1.focus();
+     inputField1.style.borderColor = 'hsl(0, 100%, 74%)';
+     inputField1.style.borderWidth = '2px';
+     errorImg1.style.display = 'block';
+     errorMsg1.style.display = 'block';
+  } else {
+     inputVal1 === '';
+     inputField1.focus();
+     errorImg1.style.display = 'none';
+     errorMsg1.style.display = 'none';
   }
-  // } 
-   else {
-    firstVal = '';
-    firstName.focus();
-    errorImg1.style.display = 'none';
-    errorMsg1.innerHTML = '';
-    // lastVal = '';
-    // lastName.focus();
-    // errorImg2.style.display = 'none';
-    // errorMsg2.innerHTML = '';
-    // emailVal = '';
-    // emailInput.focus();
-    // errorImg3.style.display = 'none';
-    // errorMsg3.innerHTML = '';
-    // password = '';
-    // passwordInput.focus();
-    // errorImg4.style.display = 'none';
-    // errorMsg4.innerHTML = '';
+  if (inputVal2 === '') {
+   inputVal2 === '';
+   inputField2.focus();
+   inputField2.style.borderColor = 'hsl(0, 100%, 74%)';
+   inputField2.style.borderWidth = '2px';
+   errorImg2.style.display = 'block';
+   errorMsg2.style.display = 'block';
+  } else {
+   inputVal2 === '';
+   inputField2.focus();
+   errorImg2.style.display = 'none';
+   errorMsg2.style.display = 'none';
   }
-});
+  if (inputVal3 === '' || !validateEmail(inputVal3)) {
+   inputVal3 === '';
+   inputField3.style.borderColor = 'hsl(0, 100%, 74%)';
+   inputField3.style.borderWidth = '2px';
+   errorImg3.style.display = 'block';
+   errorMsg3.style.display = 'block';
+  } else {
+   inputVal3 === '';
+   errorImg3.style.display = 'none';
+   errorMsg3.style.display = 'none';
+  }
+  if (inputVal4 === '') {
+   inputVal4 === '';
+   inputField4.focus();
+   inputField4.style.borderColor = 'hsl(0, 100%, 74%)';
+   inputField4.style.borderWidth = '2px';
+   errorImg4.style.display = 'block';
+   errorMsg4.style.display = 'block';
+  } else {
+   inputVal4 === '';
+   inputField4.focus();
+   errorImg4.style.display = 'none';
+   errorMsg4.style.display = 'none';
+  }
+  event.preventDefault();
+}
 
 
 // regex copied from StackOverflow //
 
-function validateEmail(emailAdd) {
-  let valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return valid.test(String(emailInput).toLowerCase());
-}
+function validateEmail(inputVal3) {
+   let valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   return valid.test(String(emailInput).toLowerCase());
+ }
